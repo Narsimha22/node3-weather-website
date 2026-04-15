44fdc6db4d0d8c72db51604389ac3245e2545586
Narsimha22 <narsimhasadula22@gmail.com>
2026-04-15 17:01:45 +0530

    Updated to use relative URLs

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,13 @@ const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg-1')
 const msgTwo = document.querySelector('#msg-2')
 
-const baseUrl = window.location.origin
-
-fetch(`${baseUrl}/weather?address=Hyderabad`)
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     msgOne.textContent = "Loading...";
     msgTwo.textContent = "";
 
-    fetch(`${baseUrl}/weather?address=`+search.value).then((response) => {
+    fetch(`/weather?address=`+search.value).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 msgOne.textContent = data.error;
